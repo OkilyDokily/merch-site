ce21d8a6a0a7b607c08aa5857ed679ec2be67d3b
me <me@me.me.com>
2021-03-29 15:59:25 -0700

    more stuff

---
+++ src/MerchController.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class MerchController extends React.Component
+{
+  constructor()
+  {
+    super();
+    this.state = {
+      currentComponent: "MerchList"
+    }
+  }
+
+  render()
+  components = {
+    
+  }
+  let currentComponent = components[this.state.currentComponent];
+
+  {
+    return(
+      <div>
+
+      </div>
+    );
+  };
+}
